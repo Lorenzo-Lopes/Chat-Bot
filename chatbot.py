53646176192093e8b2d961d7460dbb31169f71cd
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2026-04-10 22:09:43 -0300

    Primeiro codigo rodando do chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,29 @@
-#teste
+import os
+from dotenv import load_dotenv
+import google.genai as genai
+
+load_dotenv()
+MINHA_CHAVE_API = os.getenv("GEMINI_API_KEY")
+
+
+# Inicializa o cliente da nova biblioteca
+client = genai.Client(api_key=MINHA_CHAVE_API)
+
+# Inicia o chat usando o modelo mais recente
+chat = client.chats.create(model='gemini-flash-latest')
+
+print("Chatbot iniciado! Digite 'sair' para encerrar.")
+print("-*-"*15)
+
+prompt = input("Você: ")
+
+while prompt.lower() != "sair":
+    # Envia a mensagem
+    resposta = chat.send_message(prompt)
+    
+    # Imprime a resposta
+    print(f"Gemini: {resposta.text}")
+    print("-*-"*15)
+    
+    # Pede a próxima entrada
+    prompt = input("Você: ")
