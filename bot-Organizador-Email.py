c1c28aa6191b726b8821b3bf9cb39905d76f1972
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2026-04-14 23:25:20 -0300

    Correção na resposta da AI

--- bot-Organizador-Email.py
+++ bot-Organizador-Email.py
@@ -5,15 +5,15 @@ import google.genai as genai
 load_dotenv()
 MINHA_CHAVE_API = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=MINHA_CHAVE_API)
-chat = client.chats.create(model='gemini-flash-latest')
+chat = client.chats.create(model='gemini-2.5-flash')
 
 emails_organizados = []
 
 def organizar_emails(emails):
     for email in emails:
-        resposta = chat.send_message(f"Resuma esse email para mim, me diga qual o assunto e quem me mandou e me retorne um item de dicionário em python:\n\n{email}, sem mais nada além do item de dicionário, apenas o codigo em python, sem explicações, sem texto, apenas o item de dicionário em python")
-        emails_organizados.append(resposta.text)
-        print(emails_organizados)
+        resposta = chat.send_message(f"Retorne uma linha resumindo o email nesse formato (assunto: remetente - resumo):{email}")
+        
+        print(resposta.text,'\n')
     return emails_organizados
 
 organizar_emails(emails_multilinhas)
